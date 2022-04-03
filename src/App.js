d86242f0390b2aa4fe5b6352a5c5d887ab36ed2c
Hardchik <hardik.chhabra.ug20@nsut.ac.in>
2022-04-03 14:39:01 +0530

    sessional changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React, { useEffect } from 'react'
 // import { withRouter } from 'react-router-dom'
 import ReactGA from 'react-ga'
 import TagManager from 'react-gtm-module'
+import Logo from './logo/Padhakku White.png'
+import Footer from './components/Footer'
 
 const tagManagerArgs = {
     gtmId: 'GTM-KT2GFP9'
@@ -21,10 +23,14 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='logo'><img src='https://padhakku.s3.ap-south-1.amazonaws.com/website_icon_logos/Padhakku+Blue.png' alt='...' /></div>
-      <div className='poster'><img src='https://padhakku.s3.ap-south-1.amazonaws.com/website+banners/Resume.gif' alt='...' /></div>
-      <div className='content'><p>Lorem Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p></div>
-      <a className='rent_button' href='https://www.google.com/'>CLICK HERE</a>
+      <div className='logo'><img src={Logo} alt='...' /></div>
+      <div className='poster'><img src='https://padhakku.s3.ap-south-1.amazonaws.com/Marketing+landing+Pages/Rent+Page.png' alt='...' /></div>
+      <div className='content'><p>Now you can get college curriculum books at rent for as low as 50 Rs.</p></div>
+      <div className='flex'>
+        <a className='rent_button' href='https://bit.ly/padhakku-rent-ml'>RENT NOW</a>
+        <a className='whatsapp_button' href='https://wa.me/918376070107/'>WHATSAPP</a>
+      </div>
+      <Footer />
     </div>
   );
 }
